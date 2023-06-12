cb48372ed1e40ddd136c26deaf07bf21328b52c4
Jeanne <j.pietropaoli@yahoo.fr>
2023-06-12 18:10:36 +0200

    Add start-button click logic to start quizz

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import backgroundBottomCorner from "./assets/background-bottom-corner.png"
 function App() {
   const [startQuizz, setStartQuizz] = React.useState(false)
 
+  function handleStartQuizz() {
+    setStartQuizz(true)
+  }
+
   return (
     <div className="app">
       <img className="background-top-corner" src={backgroundTopCorner} alt="top-corner shape" />
       <img className="background-bottom-corner" src={backgroundBottomCorner} alt="bottom-corner shape" />
-      {startQuizz ? <Quizz /> : <Home />}
+      {startQuizz ? <Quizz /> : <Home handleStartQuizz={handleStartQuizz} />}
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Home() {
+function Home(props) {
     return(
         <div className="home">
             <h1 className="home--title">Quizzable</h1>
             <h3 className="home--description">Fun little quizz to test your skilzz</h3>
-            <button className="home--start-button">Start quizz</button>
+            <button className="home--start-button" onClick={props.handleStartQuizz}>Start quizz</button>
         </div>
     )
 }
