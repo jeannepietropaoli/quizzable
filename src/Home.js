0b69be060add783eb7d65d3cd6e1809165b9b1e1
Jeanne <j.pietropaoli@yahoo.fr>
2023-06-12 17:26:41 +0200

    Create home page elements

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div className="home">
+            <h1 className="home--title">Quizzable</h1>
+            <h3 className="home--description">Fun little quizz to test your skilzz</h3>
+            <button className="home--start-button">Start quizz</button>
+        </div>
+    )
+}
+
+export default Home
