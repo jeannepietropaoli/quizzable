c37587338cbe762fa306c7354bf3be266c2e9ce0
Jeanne <j.pietropaoli@yahoo.fr>
2023-06-12 17:30:44 +0200

    Create basic App component and Home/Quizz rendering logic

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Quizz from "./Quizz";
+import Home from "./Home";
+import backgroundTopCorner from "./assets/background-top-corner.png"
+import backgroundBottomCorner from "./assets/background-bottom-corner.png"
+
+function App() {
+  const [startQuizz, setStartQuizz] = React.useState(false)
+
+  return (
+    <div className="app">
+      <img className="background-top-corner" src={backgroundTopCorner} alt="top-corner shape" />
+      <img className="background-bottom-corner" src={backgroundBottomCorner} alt="bottom-corner shape" />
+      {startQuizz ? <Quizz /> : <Home />}
+    </div>
+  );
+}
+
+export default App;
