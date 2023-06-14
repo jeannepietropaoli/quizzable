82a8562e5382ff3a7acf203a90f8b8b3fc50d334
Jeanne <j.pietropaoli@yahoo.fr>
2023-06-14 04:50:01 +0200

    Modify favicon and add its attribution on home page

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ function Home(props) {
             <h1 className="home--title">Quizzable</h1>
             <h3 className="home--description">Fun little quizz to test your skilzz</h3>
             <button className="home--start-button" onClick={props.handleStartQuizz}>Start quizz</button>
+            <a className="favicon--attribution" href="https://www.flaticon.com/free-icons/quiz" title="quiz icons">Quiz icons created by Freepik - Flaticon</a>
         </div>
     )
 }
